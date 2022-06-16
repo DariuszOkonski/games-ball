431d37bb0c205e358dd271f94c5e63c6c0abd8c2
Dariusz Okonski <darek200180@gmail.com>
2022-06-16 12:03:51 +0100

    add ball and moving paddle

---
+++ app.js
@@ -0,0 +1,79 @@
+var ballX = 75;
+var ballY = 75;
+var ballSpeedX = 5;
+var ballSpeedY = 7;
+
+var canvas, canvasContext;
+const PADDLE_WIDTH = 100;
+const PADDLE_THICKNESS = 10;
+var paddleX = 400;
+
+function updateMousePos(evt) {
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  //   var mouseY = evt.clientY - rect.top - root.scrollTop;
+  paddleX = mouseX - PADDLE_WIDTH / 2;
+}
+
+window.onload = function () {
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext("2d");
+
+  var framesPerSecond = 30;
+  setInterval(updateAll, 1000 / framesPerSecond);
+
+  canvas.addEventListener("mousemove", updateMousePos);
+};
+
+function updateAll() {
+  moveAll();
+  drawAll();
+}
+
+function moveAll() {
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  if (ballX < 0) {
+    ballSpeedX *= -1;
+  }
+
+  if (ballX > canvas.clientWidth) {
+    ballSpeedX *= -1;
+  }
+
+  if (ballY < 0) {
+    ballSpeedY *= -1;
+  }
+
+  if (ballY > canvas.clientHeight) {
+    ballSpeedY *= -1;
+  }
+}
+
+function drawAll() {
+  colorRect(0, 0, canvas.clientWidth, canvas.clientHeight, "black");
+  colorCircle(ballX, ballY, 10, "white");
+
+  colorRect(
+    paddleX,
+    canvas.clientHeight - PADDLE_THICKNESS,
+    PADDLE_WIDTH,
+    PADDLE_THICKNESS,
+    "white"
+  );
+}
+
+function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
+  canvasContext.fillStyle = fillColor;
+  canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+}
+
+function colorCircle(centerX, centerY, radius, fillColor) {
+  canvasContext.fillStyle = fillColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
